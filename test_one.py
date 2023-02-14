2f11fdff1fcece446c340acd724b9afa1caa8eb1
mahteuso <mateus@ufscar.br>
2023-02-14 00:37:50 -0500

    create system functions

--- test_one.py
+++ test_one.py
@@ -8,4 +8,14 @@ def create_account(number, user, balance, limit):
 def extract(account):
     print(f"You balance is: {account['balance']}")
 
-def
+def cash(account, value):
+    if value > account['balance']:
+        if value <= account['limit']:
+            account['limit'] -= value
+        else:
+            print("You limit is out")
+    else:
+        account['balance'] -= value
+
+
+def deposit(account, value):
