19ed290c997d14c0938191052a63275e0cdebb0d
mahteuso <mateus@ufscar.br>
2023-02-15 00:06:58 -0500

    create class Account

---
+++ account.py
@@ -0,0 +1,11 @@
+"""
+Create the functions an account
+"""
+
+
+class Account:
+    def __int__(self, number, user, balance, limit):
+        self.number = number
+        self.user = user
+        self.balance = balance
+        self.limit = limit
