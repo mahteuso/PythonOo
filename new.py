bd0a61734c9afe05cab7c3cc62f17a0f9db138a0
mahteuso <mateus@ufscar.br>
2023-02-28 02:52:36 -0500

    create getters and setters

--- new.py
+++
@@ -1,6 +0,0 @@
-class Roger:
-
-    def __init__(self, idade, altura, peso):
-        self.idade = idade
-        self.altura = altura
-        self.peso = peso
--- series_and_films.py
+++ series_and_films.py
@@ -22,18 +22,16 @@ class Program:
 
 class Film(Program):
     def __init__(self, name, year, duration):
-        self._name = name.title()
-        self.year = year
+        super().__init__(name, year)
         self.duration = duration
-        self._likes = 0
+
 
 
 class Sitcom(Program):
     def __init__(self, name, year, seasons):
-        self._name = name.title()
-        self.year = year
+        super().__init__(name, year)
         self.seasons = seasons
-        self._likes = 0
+
 
 
 avangers = Film('Avangers - infinity war', 2018, 160.0)
@@ -45,6 +43,5 @@ avangers.new_likes()
 the_it_crowd.new_likes()
 the_it_crowd.new_likes()
 the_it_crowd.new_likes()
-print(f'Name: {avangers.name}, Year: {avangers.year}, Duration: {avangers.duration} min, Likes: {avangers.likes}')
-print(
-    f'Name: {the_it_crowd.name}, Year: {the_it_crowd.year}, Seasons: {the_it_crowd.seasons}, Likes: {the_it_crowd.likes}')
+print(f'{avangers.name} - {avangers.year}: {avangers.likes}')
+print(f'{the_it_crowd.name} - {the_it_crowd.seasons}: {the_it_crowd.likes}')
