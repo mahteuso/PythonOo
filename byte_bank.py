ea2206f89b02353fd6ac35c645e08a2fd4c23216
mahteuso <mateus@ufscar.br>
2023-03-10 09:55:16 +0000

    studying error propagation

--- byte_bank.py
+++ byte_bank.py
@@ -10,12 +10,49 @@ class CurrentAccount:
     total_accounts_created = 0
     transfer_rate = None
 
-    def __init__(self, customer, agency, number):
+    def __init__(self, customer, agency, number, balance, limit):
         self.customer = customer
-        self.agency = agency
-        self.number = number
+        self.__set_agency(agency)
+        self.__number = number
+        self.__balance = balance
+        self.__set_limit(limit)
         CurrentAccount.total_accounts_created += 1
         CurrentAccount.transfer_rate = 30 / CurrentAccount.total_accounts_created
+
+    @property
+    def limit(self):
+        return self.__limit
+
+    def __set_limit(self, value):
+        if not isinstance(value, int):
+            raise ValueError('The limit accepts only integers')
+        if value <= 0:
+            raise ValueError('The limit accepts only numbers greater than zero')
+        self.__limit = value
+
+    @property
+    def balance(self):
+        return self.__balance
+
+
+    def __set_balance(self, value):
+        if not isinstance(value, int):
+            raise ValueError("The balance accepts only integers")
+        if value <= 0:
+            raise ValueError('The balance accepts only numbers greater than zero')
+        self.__balance = value
+
+    @property
+    def agency(self):
+        return self.__agency
+
+
+    def __set_agency(self, value):
+        if not (isinstance(value, int)):
+            raise ValueError('Agency accepts only integers')
+        if value <= 0:
+            raise ValueError("Agency accepts only numbers greater than 0")
+        self.__agency = value
 
     def transfer(self, value, destiny):
         pass
@@ -23,27 +60,15 @@ class CurrentAccount:
     def cash(self, value):
         pass
 
-    def balance(self):
+    def balance_out(self):
         pass
 
     def deposity(self, value):
         pass
 
-def to_divide(dividend, divider):
-    #print(divider.result)
-    return dividend / divider
+account1 = CurrentAccount('Mateus', 32, 123, 50, 1000)
+print(account1.agency)
+account1.agency = 1
 
 
-def test_divider(divider):
-    try:
-        result = to_divide(10, divider)
-        print(result)
-    except ZeroDivisionError:
-        print("Erro na divisão por zero")
-    except TypeError:
-        print("entre apenas com números")
 
-try:
-    test_divider('a')
-except AttributeError:
-    print('Erro de atributo')
