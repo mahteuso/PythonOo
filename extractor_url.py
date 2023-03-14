d3c47e091c8c17819c95861ab9e760b2f54e46f6
mahteuso <mateus@ufscar.br>
2023-03-14 15:03:21 +0500

    create a url

--- extractor_url.py
+++ extractor_url.py
@@ -1,8 +1,8 @@
 url = 'https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real'
 
+
 # getting the reference
 question_mark = url.find('?')
-new_mark = url.find('&')
 
 # separating terms
 url_base = url[:question_mark]
@@ -11,24 +11,18 @@ url_parameters = url[question_mark+1:]
 print(url_parameters)
 
 # getting the value of a parameter
-search_parameter = 'moedaOrigem'
+search_parameter = input('Enter a parameter: ')
 index_search_parameter = url.find(search_parameter)
-
 index_word = index_search_parameter + (len(search_parameter) + 1)
 search_word = url[index_word:]
+new_mark = url.find('&', index_word)
 
-parameter_one = url[question_mark+1:new_mark]
-parameter_two = url[new_mark+1:]
-print(f'\n{parameter_two}')
-print(search_word)
-
-
-new_search = 'moedaDestino'
-index_new_search = url.find('moedaDestino')
-
-len_new_search = index_new_search + (len(new_search) + 1)
-parameter_dolar = url[len_new_search:new_mark]
-print(f'\n{parameter_one}')
-print(parameter_dolar)
-
-
+if new_mark == -1:
+    parameter_two = url[index_search_parameter:]
+    print(f'\n{parameter_two}')
+    print(search_word)
+else:
+    parameter_one = url[question_mark + 1:new_mark]
+    parameter_dolar = url[index_word:new_mark]
+    print(f'\n{parameter_one}')
+    print(parameter_dolar)
---
+++ test_two.py
@@ -0,0 +1,10 @@
+Roger = 'Rogerio'
+print(Roger[1])
+mateus = Roger
+#Roger[1] = 'm'
+print(mateus)
+
+roger = ['r', 'o']
+print(roger)
+roger[1] = 'n'
+print(roger)
