06c1af9d34ea217589c1d4223f85faeb584c875f
mahteuso <mateus@ufscar.br>
2023-03-23 03:57:58 +0000

    url validator

--- cole.py
+++ cole.py
@@ -1,8 +1,10 @@
 from collections import defaultdict, Counter
+import time
 
 
-lista = [3, 2, 1, 5, 8, -1]
+lista = [3, 2, 1]
 lista_2 = [5, 4, 3, -1, 10]
+'''
 print(set(lista) ^ set(lista_2))
 
 phrases = 'Tomorrow I go to the shopping and after I will play soccer with my friends'
@@ -16,31 +18,49 @@ print(letter_dic)
 print(Counter(new_phrases))
 
 print(Counter('banana'))
-
-
-
 '''
+
+lista = list(range(10000000))
+lista.reverse()
+t1 = time.time()
+print('using function native sort()')
+lista.sort()
+#print(lista)
+tempoExec = time.time() - t1
+print(f"Tempo de execução: {tempoExec} segundos")
+
+t1 = time.time()
+print('')
+# código aqui
+print('first ordination')
+lista = list(range(10000))
+lista.reverse()
 for i, l in enumerate(lista):
     for j, m in enumerate(lista):
         if l < m:
             temp = lista[i]
             lista[i] = lista[j]
             lista[j] = temp
-            print(lista)
-    print("out second for")
-    print(lista)
+#print(lista)
 
+
+print('\n')
+tempoExec = time.time() - t1
+print(f"Tempo de execução: {tempoExec} segundos")
+
+print('')
 print('Second ordination:')
-
+lista = list(range(10000))
+lista.reverse()
+t1 = time.time()
 for i in range(len(lista)):
-    for j, m in enumerate(lista):
+    for j, _ in enumerate(lista):
+
         if lista[i] < lista[j]:
             temp = lista[i]
             lista[i] = lista[j]
             lista[j] = temp
-            print(lista)
-    print('out second for')
-    print(lista)
-
-'''
+#print(lista)
+tempoExec = time.time() - t1
+print(f"Tempo de execução: {tempoExec} segundos")
 
